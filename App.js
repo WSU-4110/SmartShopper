e61cfc746d50361e754f87df69dbaab4b76552df
Adrian-Tarnowski <77858011+Adrian-Tarnowski@users.noreply.github.com>
2021-02-25 21:26:19 -0500

    Updated Homescreen

--- App.js
+++ App.js
@@ -1,64 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, ImageBackground,SafeAreaView} from 'react-native';
-import { Button, SearchBar } from 'react-native-elements';
+import React, { Component} from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import CustomButton from '../demo/App code/CustomButton';
 
-export default function App() {
-  
-  console.log('hello');
+
+export default class App extends Component {
+
+  render(){
   
   return (
-    <View justifyContent='space-between' height='100%' style={back.container}>
-      <Text style={styles.ShopperTitle}>Smart Shopper</Text>
-      <Button  color="#f194ff" title='Create New List'/>
-      <Button style={styles.buttonStuff} title='View Previous Lists'/> 
-      <Button title='Expiration Date Tracker' />
-      <Button title='Recommended Items' />
-      <Button title='Notifications' />
-      <Text>                                    </Text>
-      <Text>                                    </Text>
-      <Text>                                    </Text>
-      <Text>Search bar maybe goes here maybe?</Text>
-      <StatusBar style="auto" />
+    <View style={title.background}>
+      <Text style={title.ShopperTitle}>SmartShopper</Text>
+  
+    <CustomButton text='Add Item' color='#222222'/>
+    <CustomButton text='View History' color='#222222'/>
+    <CustomButton text='Sort Items' color='#222222'/>
+    <CustomButton text='Expiration Dates' color='#222222'/>
     </View>
-
   );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff3e0',
+    backgroundColor: '#222222',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  ShopperTitle:{
-  textAlign:'center',
-    fontSize:50,
-    color:'#4e93e0',
-  },
-  SearchBarPadding:{
-    width:'100%',
-    height: 20,
-    backgroundColor: 'white',
-    borderRadius:8,
-    },
-    SearchBar:{
-    width:'100%',
-    height:'100%',
-    paddingLeft:8,
-    fontSize:8,
-    },
-    buttonStuff:
-    {
-      padding:10,
-      backgroundColor:'red',
-      color:'red',
-    }
+    padding:200
+  }
   });
 
-  const back = StyleSheet.create({
-    container: {
-      backgroundColor: '#fff3e0',
+  const title = StyleSheet.create({
+    background: {
+      backgroundColor: 'coral',
+      height:'100%',
+      width: '100%'
+      
+    },
+    ShopperTitle:{
+      marginTop:15,
+      textAlign:'center',
+        fontSize:35,
+        color:'white',
+        padding:20
     }
     });
