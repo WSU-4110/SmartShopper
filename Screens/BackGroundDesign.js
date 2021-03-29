c8f04bb066349478fdd78e55452233d7d7d98dd7
Adrian-Tarnowski <77858011+Adrian-Tarnowski@users.noreply.github.com>
2021-03-29 16:03:45 -0400

    uploading backgroundDesign
    Background has a background design

---
+++ Screens/BackGroundDesign.js
@@ -0,0 +1,8 @@
+import{StyleSheet} from 'react-native';
+
+export const BGDesign = StyleSheet.create({
+    background: {
+        backgroundColor:"coral",
+        height: "100%",
+        width: "100%",
+    }});
