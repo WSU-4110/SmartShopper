bbc729b1abfd3018ae86cff1644e1f3de261d3b3
Adrian-Tarnowski <77858011+Adrian-Tarnowski@users.noreply.github.com>
2021-03-29 15:58:01 -0400

    updating custombutton.js with Composite desgin
    Homescreen has a button (custombuttons).

--- App code/CustomButton.js
+++ App code/CustomButton.js
@@ -1,25 +1,14 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import {ButtonDesign} from "./CustomButtonDesign.js";
 
 const CustomButton = (props) => {
   const content = (
-    <View style={[styles.button, { backgroundColor: props.color }]}>
-      <Text style={styles.text}>{props.text}</Text>
+    <View style={[ButtonDesign.button, { backgroundColor: props.color }]}>
+      <Text style={ButtonDesign.text}>{props.text}</Text>
     </View>
   );
   return <TouchableOpacity onPress={props.onPress}>{content}</TouchableOpacity>;
 };
-
-const styles = StyleSheet.create({
-  button: {
-    padding: 42, //can be adjustible to fit screen depending on how many buttons are present on the homescreen
-    width: "100%",
-    alignItems: "center",
-  },
-  text: {
-    color: "white",
-    fontSize: 22,
-  },
-});
 
 export default CustomButton;
