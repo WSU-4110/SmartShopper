15f91df8233a9ee28306cdab8e73315820ec2cee
Adrian-Tarnowski <77858011+Adrian-Tarnowski@users.noreply.github.com>
2021-03-29 16:00:09 -0400

    Uploading button design to create hierarchal tree structure.
    CustomButton has a button design.

---
+++ App code/CustomButtonDesign.js
@@ -0,0 +1,13 @@
+import{StyleSheet} from 'react-native';
+
+export const ButtonDesign = StyleSheet.create({
+    button: {
+        padding: 42, //can be adjustible to fit screen depending on how many buttons are present on the homescreen
+        width: "100%",
+        alignItems: "center",
+      },
+      text: {
+        color: "white",
+        fontSize: 22,
+      },
+    });
